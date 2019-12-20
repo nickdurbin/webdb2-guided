0d2b686246d2a27f5d8fb3f3c86aed40c2da2eb3
ndurbin <nick.durbin@gmail.com>
2019-12-20 02:24:04 -0500

    I added the dotenv package and created the env file for the variables. Setup a few other server checks.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express")
 const helmet = require("helmet")
 const fruitsRouter = require("./fruits/fruits-router")
@@ -10,6 +11,16 @@ server.use(helmet())
 server.use(express.json())
 
 server.use("/api/fruits", fruitsRouter)
+
+server.get("/", (req, res) => {
+	res.send("<h1>I am your server. Yes, I am running!</h2>")
+})
+
+server.use((req, res) => {
+	res.status(404).json({ message: "You have drifted into the ether. Turn back now!"})
+})
+
+
 server.use((err, req, res, next) => {
 	console.log(err)
 	res.status(500).json({
